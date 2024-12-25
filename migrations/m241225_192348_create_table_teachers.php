30a9062acb36cb00f79a841a78f811b970b526c5
Gladkov Artem <gladkoff.artem@gmail.com>
2024-12-25 23:02:20 +0300

    create db

---
+++ migrations/m241225_192348_create_table_teachers.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241225_192348_create_table_teachers
+ */
+class m241225_192348_create_table_teachers extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%teachers}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(10)->notNull(),
+            'surname' => $this->string(10)->notNull(),
+            'gender' => $this->string(10)->notNull(),
+            'age' => $this->tinyInteger(2)->notNull(),
+            'degree' => $this->string(20)->notNull(),
+            'experience' => $this->tinyInteger(2)->notNull(),
+            'salary' => $this->tinyInteger(10)->notNull(),
+            'created_at' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()',
+            'updated_at' => 'TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP()'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%teachers}}');
+    }
+}
